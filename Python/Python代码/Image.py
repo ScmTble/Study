962dea0adb29a217d7c79b15787528f4b96589d7
ScmTble <56188521+ScmTble@users.noreply.github.com>
2020-05-19 22:56:56 +0800

    Update Image.py

--- Python/Python代码/Image.py
+++ Python/Python代码/Image.py
@@ -1,10 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+#python批量爬取图片
+import requests #requests库
+from bs4 import BeautifulSoup #“美丽汤库”
 main_html = requests.get('https://www.umei.cc/meinvtupian/meinvxiezhen/8.htm')
 main_html.encoding = 'utf-8'
 main_page = BeautifulSoup(main_html.text, "html.parser")
 a_list = main_page.find("div", class_="TypeList").find_all(
-    "a", class_="TypeBigPics")
+    "a", class_="TypeBigPics")#通过HTML知识获取元素
 n = 1
 for a in a_list:
     child_url = a.get("href")
@@ -16,6 +17,6 @@ for a in a_list:
         # 下载图片
         jpg_src = img.get("src")
         with open(f"./Image/lookk{n}.jpg", 'wb') as f:
-            f.write(requests.get(jpg_src).content)
+            f.write(requests.get(jpg_src).content)#图片下载
         print("下完第%d张" % n)
         n += 1
